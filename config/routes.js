a4126068ae320c43680a26ba10bea8f5a692b782
noxasaxon <34722463+noxasaxon@users.noreply.github.com>
2018-08-13 01:47:23 -0700

    MVP complete

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const { authenticate, generateToken } = require('./middlewares');
 
-const { authenticate } = require('./middlewares');
+const db = require('../database/dbConfig.js');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -10,10 +12,45 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  const user = req.body;
+  user.password = bcrypt.hashSync(user.password, 14);
+  db('users')
+    .insert(user)
+    .then(ids => {
+      db('users')
+        .where({ id: ids[0] })
+        .first()
+        .then(user => {
+          // req.session.username = user.username;
+          const token = generateToken(user);
+
+          res.send(token);
+          // res.status(201).json(user);
+        });
+    })
+    .catch(err => {
+      res.status(500).json({ err });
+      console.log(err);
+    });
 }
 
 function login(req, res) {
   // implement user login
+  db('users')
+    .where({ username: credentials.username })
+    .first()
+    .then(user => {
+      //check for pw match
+      if (user && bcrypt.compareSync(credentials.password, user.password)) {
+        const token = generateToken(user);
+
+        //attach token to response
+        res.send(token);
+      } else {
+        res.status(401).json({ error: 'wrong password' });
+      }
+    })
+    .catch(err => res.status(500).json({ err }));
 }
 
 function getJokes(req, res) {
--- server.js
+++ server.js
@@ -5,16 +5,19 @@ const configureRoutes = require('./config/routes');
 
 const server = express();
 const corsOptions = {
-  // If you're moving onto the stretch problem you'll need to set this obj with the appropriate fields
+  // If you're moving onto the stretch problem you'll need to set
+  // this obj with the appropriate fields
   // ensure that your client's URL/Port can achieve a Handshake
   // then pass this object to the cors() function
 };
-
+server.get('/', (req, res) => {
+  res.sendStatus(201).json({ message: 'up and running' }); //throws error for modifying headers after send
+});
 server.use(express.json());
 server.use(cors());
 
 configureRoutes(server);
 
 module.exports = {
-  server,
+  server
 };
